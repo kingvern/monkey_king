f0f211e1e3c5fc69d1e419f0137c8112c29590ee
王凯 <wkorz@wangkaideMacBook-Pro.local>
2018-09-14 13:38:18 +0800

    init

--- src/App.js
+++ src/App.js
@@ -2,7 +2,43 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { web3 } from "wallet";
+const abi = require("./abi/MonkeyCore").abi;
+const main = web3.loadContract(
+  abi,
+  "0x67da03db2943387c613439fba2375e18f8cf1e13"
+);
+console.log(main);
+console.log("levellength: ");
+console.log(main.GetLevelLength());
+
+
+
+
+
+
+
+
 class App extends Component {
+  getLevelCount = async () => {
+    let levelCount = await main.GetLevelLength();
+    console.log("levellength: ");
+    console.log(levelCount); 
+}
+  handleClick() {
+    this.getLevelCount();
+    }
+/** 
+  getLevelCount: async function() {
+        let levelCount = await main.GetLevelLength();
+        console.log("levelCount:"+levelCount);
+      }
+      
+  handleClick() {
+      this.getLevelCount();
+  }
+  */
+
   render() {
     return (
       <div className="App">
@@ -13,6 +49,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClicke={this.handleClick}>点击我执行getLevelCount</button>
       </div>
     );
   }
